ef488775d7cf8cc17efb7c390c6b0cf178ea5992
Edgar <edgar_giang@hotmail.com>
2016-05-15 19:13:25 -0400

    Fixed the camera not following player.

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -15,12 +15,7 @@ public class Camera : MonoBehaviour {
 
 	void Update () {
 
-        Vector3 pos = transform.position;
-
-
-        pos.z = player.position.z;
-        pos.x = player.position.x;
-        pos.y = player.position.y;
+        transform.position = player.position + distance;
 
         
     }
